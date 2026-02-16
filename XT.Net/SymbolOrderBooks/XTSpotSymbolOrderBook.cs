c54aac51c94e54352a1025c0ca4c786ff7ec5ee8
Jkorf <jankorf91@gmail.com>
2026-02-16 10:21:10 +0100

    Fixed SymbolOrderBook websocket subscription not getting closed if when waiting for initial data times out

--- XT.Net/SymbolOrderBooks/XTSpotSymbolOrderBook.cs
+++ XT.Net/SymbolOrderBooks/XTSpotSymbolOrderBook.cs
@@ -100,6 +100,9 @@ namespace XT.Net.SymbolOrderBooks
             else
             {
                 var setResult = await WaitForSetOrderBookAsync(_initialDataTimeout, ct).ConfigureAwait(false);
+                if (!setResult)
+                    await subResult.Data.CloseAsync().ConfigureAwait(false);
+
                 return setResult ? subResult : new CallResult<UpdateSubscription>(setResult.Error!);
             }
 
