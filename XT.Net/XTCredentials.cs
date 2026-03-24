5c164a27f899f0c40b74d3d6fa0eead9a83501bc
Jkorf <jankorf91@gmail.com>
2026-03-24 16:32:36 +0100

    Fix issue in credentials copying

--- XT.Net/XTCredentials.cs
+++ XT.Net/XTCredentials.cs
@@ -43,5 +43,8 @@ namespace XT.Net
             Secret = secret;
             return this;
         }
+
+        /// <inheritdoc />
+        public override ApiCredentials Copy() => new XTCredentials(this);
     }
 }
