98d441e4fbb16cc0616c4e7e7d81fbb54c9cd1e2
Jkorf <jankorf91@gmail.com>
2026-08-13 13:34:31 +0200

    Update SharedRestIntegrationTests.cs

--- XT.Net.UnitTests/SharedRestIntegrationTests.cs
+++ XT.Net.UnitTests/SharedRestIntegrationTests.cs
@@ -10,7 +10,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using XT.Net.Interfaces.Clients.FuturesApi;
-
+ 
 namespace XT.Net.UnitTests
 {
     [TestFixture]
